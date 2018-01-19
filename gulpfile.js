47510645b2daec93e08a7290d870693a83eb5adf
何俊洁 <381548325@qq.com>
2018-01-19 18:08:51 +0800

    'update'

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-//const nodemon = require('nodemon');
 const nodemon = require('gulp-nodemon');
+const clean = require('gulp-clean');
+const express = require('gulp-express');
+const gls = require('gulp-live-server');
+const livereload = require('gulp-livereload');
 const sass = require('gulp-sass');
 const path = require('path');
 const del = require('del');
@@ -20,9 +23,9 @@ const reload = browserSync.reload;
  */
 
 gulp.task('sass',()=> {
-	return gulp.src(['./src/assets/sass/**/*.scss', './src/assets/sass.scss'])
+	return gulp.src('./src/assets/sass/**.scss')
 	.pipe(plumber())
-	/*.pipe(autoprefixer('last 6 version'))
+	.pipe(autoprefixer('last 3 version'))
 	.pipe(sass().on('error', (err)=> {
 		console.log('sass err:', err);
 	}))
@@ -31,9 +34,9 @@ gulp.task('sass',()=> {
 	.pipe(rename((path)=> {
 		path.basename += ".min"
 	}))
-	.pipe(cssbase64())*/
+	.pipe(cssbase64())
 	.pipe(gulp.dest('dist/assets/css'))
-	.pipe(browserSync.stream())
+	.pipe(livereload())
 });
 
 /**
@@ -42,15 +45,15 @@ gulp.task('sass',()=> {
 
 gulp.task('css', ()=> {
 	return gulp.src(['src/assets/css/**/*.css', 'src/assets/css/*.css'])
-	/*.pipe(plumber())
+	.pipe(plumber())
 	.pipe(autoprefixer('last 6 version'))
 	.pipe(cssbase64())
 	.pipe(cssnano())
 	.pipe(rename((path)=> {
 		path.basename += ".min"
-	}))*/
+	}))
 	.pipe(gulp.dest('dist/assets/css'))
-	.pipe(browserSync.stream())
+	.pipe(livereload())
 });
 
 /**
@@ -58,15 +61,16 @@ gulp.task('css', ()=> {
  */
 
 gulp.task('html', ()=> {
-	return gulp.src(['src/*.html', 'src/views/*.html'])
+	return gulp.src(['src/**.html', 'src/**/*.html'])
 	/*.pipe(plumber())
 	.pipe(fileInclude({
 		prefix: '@@',
 		baspath: '@file'
 	}))*/
 	.pipe(gulp.dest('dist'))
-	.pipe(browserSync.stream())
+	.pipe(livereload())
 });
+
 
 /**
  * js
@@ -74,39 +78,51 @@ gulp.task('html', ()=> {
 
 gulp.task('js', (path)=> {
 	return gulp.src(['src/assets/js/**/*.js', 'src/assets/js/*.js'])
-	/*.pipe(plumber())
+	.pipe(plumber())
 	.pipe(babel({
 		presets: ['env']
-	}))*/
+	}))
 	.pipe(gulp.dest('./dist/assets/js'))
-	/*.pipe(uglify())
+	.pipe(uglify())
 	.pipe(rename((path)=> {
 		path.basename += '.min'
 	}))
-	.pipe(gulp.dest('dist/assets/js'))*/
-	.pipe(browserSync.stream())
+	.pipe(gulp.dest('dist/assets/js'))
+	.pipe(livereload())
+});
+
+/**
+ * watch
+ */
+
+gulp.task('watch', ()=> {
+	livereload.listen({start: true});
+	gulp.watch('src/**.html', ['html']);
+	gulp.watch('src/views/**.html', ['html']);
+	gulp.watch('src/assets/css/**.css', ['css']);
+	gulp.watch('src/assets/css/**/**.css', ['css']);
+	gulp.watch('src/assets/js/**.js', ['js']);
+	gulp.watch('src/assets/js/**/**.js', ['js']);
+	// gulp.watch('src/**/*.*',(file)=> {
+ //        livereload.changed(file.path);
+ //    });
+ 	gulp.watch(['./**.*','./dist/**/*']).on('change', reload);
 });
 
 /**
  * server
- * port 7000 proxy express port 3001
  */
 
-gulp.task('browserSync', ['nodemon'], ()=> {
-	let files = [
-		'src/**.html', 
-		'src/views/**.html', 
-		'src/assets/**/**.*', 
-		'src/assets/**/**/**.*'
-	]
-	browserSync.init(files, {
-		proxy: 'http://127.0.0.1:3001',
-		notify: false,
-		port: 7000
-	});
+gulp.task('server', ['nodemon', 'sass', 'js', 'css', 'html', 'watch'], ()=> {
+	// let server = gls.new('server.js');
+	// server.start();
 
-	gulp.watch(files).on('change', reload);
-});
+	// gulp.watch(['src/**.html', 'src/views/**.html', 'src/assets/css/**.css', 'src/assets/css/**/**.css', 'src/assets/js/**.js', 'src/assets/js/**/**.js'], (file)=> {
+	// 	server.notify.apply(server, [file]);
+ //    });
+
+    //gulp.watch('server.js', server.start.bind(server));
+})
 
 /**
  * open express server
@@ -127,6 +143,7 @@ gulp.task('nodemon', (cb)=> {
 /**
  * build打包
  */
+
 gulp.task('build', ['html', 'css', 'sass', 'js'], ()=> {
 	//pass
 });
@@ -136,5 +153,5 @@ gulp.task('build', ['html', 'css', 'sass', 'js'], ()=> {
  * default
  */
 
-gulp.task('default', ['browserSync']);
+gulp.task('default', ['server']);
 
