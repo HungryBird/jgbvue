08982b79d28c9782bc0f4bbcc106ea5fa06d7231
何俊洁 <381548325@qq.com>
2018-02-02 16:27:01 +0800

    '封装系统参数js'

--- dist/assets/js/views/systemSettings/systemParameter.js
+++ dist/assets/js/views/systemSettings/systemParameter.js
@@ -1,35 +1,52 @@
-new Vue({
-	el: '#app',
-	data: {
-		table: []
-	},
-	mounted: function() {
-		const _self = this;
-		axios.get('/systemParameter/data_get').then((req)=> {
-			let jdata = JSON.parse(req.data.message);
-			_self.table = jdata;
-		}).catch((err)=> {
-			console.log('err', err);
-		})
-	},
-	methods: {
-		save() {
-			var _self = this;
-			axios.post('/systemParameter/data_save', _self.table).then((req)=>{
-				if(req.data.status) {
-					_self.$message({
-						type: 'success',
-						message: req.data.message
-					})
-				}else{
-					_self.$message({
-						type: 'error',
-						message: req.data.message
+JGBVue = {
+	module: {}
+};
+
+JGBVue.module.systemParameter = ()=>{
+	const _this = {}
+	,that = {};
+
+	_this.init = (getDataUrl, saveDataUrl)=> {
+		new Vue({
+			el: '#app',
+			data: {
+				table: []
+			},
+			mounted: function() {
+				const _self = this;
+				axios.get(getDataUrl).then((req)=> {
+					let jdata = JSON.parse(req.data.message);
+					_self.table = jdata;
+				}).catch((err)=> {
+					console.log('err', err);
+				})
+			},
+			methods: {
+				save() {
+					var _self = this;
+					axios.post(saveDataUrl, _self.table).then((req)=>{
+						if(req.data.status) {
+							_self.$message({
+								type: 'success',
+								message: req.data.message
+							})
+						}else{
+							_self.$message({
+								type: 'error',
+								message: req.data.message
+							})
+						}
+					}).catch((err)=>{
+						console.log('err', err);
 					})
 				}
-			}).catch((err)=>{
-				console.log('err', err);
-			})
-		}
+			}
+		});
 	}
-});
+
+	that.init = (getDataUrl, saveDataUrl)=> {
+		_this.init(getDataUrl, saveDataUrl);
+	}
+
+	return that;
+}
